b6be3e7eb74d555da7eed1c8acca38309df45ae1
Aaron Valera <aaronrvalera@gmail.com>
2026-02-01 03:59:24 -0400

    Addition left biomes

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
 const btnLeft = document.querySelector(".btn-left"),
       btnRight = document.querySelector(".btn-right"),
       imgSlider = document.querySelector("#slider"),
-      sliderSection = document.querySelectorAll(".slider-section");
+      sliderSection = document.querySelectorAll(".slider-section"),
+      containers = document.querySelectorAll(".carousel-container");
 
-btnLeft.addEventListener("click", e => moveLeft());
-btnRight.addEventListener("click", e => moveRight());
+containers.forEach(container => {
+    const btnLeft = container.querySelector(".btn-left");
+    const btnRight = container.querySelector(".btn-right");
+    const imgSlider = container.querySelector(".carousels");
+    const sliderSections = container.querySelectorAll(".slider-section");
+    
+    let counter = 0;
+    let widthImg = 100 / sliderSections.length;
+    
+    btnRight.addEventListener("click", () => {
+        if (counter >= sliderSections.length - 1) {
+            counter = 0;
+        } else {
+            counter++;
+        }
+        updateSlider();
+    });
+    
+    btnLeft.addEventListener("click", () => {
+        if (counter <= 0) {
+            counter = sliderSections.length - 1;
+        } else {
+            counter--;
+        }
+        updateSlider();
+    });
+    
+    btnLeft.addEventListener("click", () => {
+        if (counter <= 0) {
+            counter = sliderSections.length - 1;
+        } else {
+            counter--;
+        }
+        updateSlider();
+        });
 
-let operation = 0,
-    counter = 0,
-    widthImg = 100 / sliderSection.length;
-
-function moveRight() {
-    if (counter >= sliderSection.length-1) {
-        operation = 0;
-        counter = 0;
-        imgSlider.style.transform = `translate(-${operation}%)`;
-        imgSlider.style.transtion = "none";
-        return
+function updateSlider() {
+    let operation = counter * widthImg;
+    imgSlider.style.transform = `translateX(-${operation}%)`;
+    imgSlider.style.transition = "all ease 0.6s";
     }
-    operation = operation + widthImg;
-    imgSlider.style.transform = `translate(-${operation}%)`;
-    counter++;
-    imgSlider.style.transition = "all ease 0.6";
-}
-
-function moveLeft() {
-    counter--;
-    if (counter < 0) {
-        counter = sliderSection.length-1;
-        operation = widthImg * (sliderSection.length-1);
-        imgSlider.style.transform = `translate(-${operation}%)`;
-        imgSlider.style.transition = "none";
-        return;
-    }
-    operation = operation - widthImg;
-    imgSlider.style.transform = `translate(-${operation}%)`;
-    imgSlider.style.transition= "all ease 0.6s";
-}
+});
