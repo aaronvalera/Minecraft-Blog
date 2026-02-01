cbece786e2e0807b4489f314f723bb4bb4febf69
Aaron Valera <aaronrvalera@gmail.com>
2026-02-01 04:12:59 -0400

    Update script

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const btnLeft = document.querySelector(".btn-left"),
-      btnRight = document.querySelector(".btn-right"),
-      imgSlider = document.querySelector("#slider"),
-      sliderSection = document.querySelectorAll(".slider-section"),
-      containers = document.querySelectorAll(".carousel-container");
+const containers = document.querySelectorAll(".carousel-container");
 
 containers.forEach(container => {
     const btnLeft = container.querySelector(".btn-left");
@@ -10,6 +6,12 @@ containers.forEach(container => {
     const imgSlider = container.querySelector(".carousels");
     const sliderSections = container.querySelectorAll(".slider-section");
     
+    function updateSlider() {
+        let operation = counter * widthImg;
+        imgSlider.style.transform = `translateX(-${operation}%)`;
+        imgSlider.style.transition = "all ease 0.6s";
+        }
+        
     let counter = 0;
     let widthImg = 100 / sliderSections.length;
     
@@ -30,19 +32,5 @@ containers.forEach(container => {
         }
         updateSlider();
     });
-    
-    btnLeft.addEventListener("click", () => {
-        if (counter <= 0) {
-            counter = sliderSections.length - 1;
-        } else {
-            counter--;
-        }
-        updateSlider();
-        });
 
-function updateSlider() {
-    let operation = counter * widthImg;
-    imgSlider.style.transform = `translateX(-${operation}%)`;
-    imgSlider.style.transition = "all ease 0.6s";
-    }
 });
